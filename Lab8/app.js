87e08d9e6007444e3a012cf6fb031adbeb9fd917
Manuel Villalpando Linares <A01352033@tec.mx>
2023-02-23 12:23:24 -0600

    Lab8

---
+++ Lab8/app.js
@@ -0,0 +1,7 @@
+console.log("hola desde node");
+
+
+//El modulo filesystem surve para acceder al sistema de archivo de la computadora
+const filesystem = requires('fs');
+
+//filesystem.writefileSync('hola.txt', 'hola desde node');
