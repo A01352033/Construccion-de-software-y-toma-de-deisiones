9fb15e4572069b4ef0275a30d0ce4e73a7021c8c
Manuel Villalpando Linares <A01352033@tec.mx>
2023-02-24 10:17:47 -0600

    Lab8

--- Lab8_BACK-END/lab.js
+++ Lab8_BACK-END/lab.js
@@ -17,6 +17,17 @@ function prom (array) {
 console.log(prom([1, 2, 3, 4, 5, 6, 7, 8]))
 console.log(prom([]))
 
+//El modulo filesystem surve para acceder al sistema de archivo de la computadora
+const filesystem = require("fs");
+
+filesystem.writeFileSync('Texto_Lab.txt', 'Escribí este texto desde lab.js');
+
+
+function voltear(oracion) {
+    return oracion.split(" ").reverse().join(" ");
+}
+var lol = voltear("hola me llamo manuel y hablo alrevés");
+
 
 const fs = require('fs');
 
@@ -29,6 +40,7 @@ fs.readFile("../Lab1_HTML/index.html",  function (err, html) {
     const server = pag.createServer((request, response) => {
         console.log(request.url);
         response.setHeader('Content-Type', 'text/html');
+        response.write(lol);
         response.write(html);
         response.end();
     })
