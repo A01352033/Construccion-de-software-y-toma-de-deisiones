523e7c1c3b9efc39d28e687236c2c179e301e7a6
Manu Villa <Manuel Villalpando Linares>
2023-03-08 15:36:20 -0600

    Lab4_JS

--- Lab4_JS/script.js
+++ Lab4_JS/script.js
@@ -31,3 +31,60 @@ function suma() {
     };
 };
 
+function contador(arr) {
+    let neg = 0;
+    let cero = 0;
+    let pos = 0;
+    for(let i=0;arr.length>i;i++) {
+        if(arr[i]<0) {
+            neg++;
+        };
+        if(arr == 0) {
+            cero ++;
+        };
+        if(arr[i]>0) {
+            pos++;
+        };
+    };
+    return neg + " " + cero + " " + pos;
+};
+let arr = [7, 23, -6, 0, 3, 0];
+let tot = contador(arr);
+document.write( "<br><br>" +
+"Arreglo de numeros: " + arr + "<br>" +
+"La cantidad de numeros negativos es: " + tot[0] + "<br>" +
+"La cantidad de numeros cero es: " + tot[2] + "<br>" +
+"La cantidad de numeros positivos es: " + tot[4] + "<br>");
+
+
+
+function promedio(arrNum) {
+    for (let i = 0; i < arrNum.length; i++) {
+        for (let j = 0; j < arrNum[i].length; j++) {
+            row += arrNum[i][j];
+        };
+        if(i == 0) {
+            promedio1 = row/arrNum[i][j].length
+        };
+        if(i == 1) {
+            promedio2 = row/arrNum[i][j].length
+        };
+        if(i == 2) {
+            promedio3 = row/arrNum[i][j].length
+        };
+    };
+    return promedio1 + " " + promedio2 + " " + promedio3;
+};
+
+let list = [[1, 2, 3], [5, 6, 7], [9, 10, 11]];
+let prom = promedio(list);
+document.write( "<br><br>" +
+"Matriz utilizada: " + list + "<br>" +
+"Promedio de la primer fila: " + prom[0] + "<br>" +
+"Promedio de la segunda fila: " + prom[2] + "<br>" +
+"Promedio de la tercera fila: " + prom[4] + "<br>");
+
+
+function inverso(num) { 
+
+}
