6ac83efc5be334d7a91e55b97c5bf37f7867b523
Manuel Villalpando Linares <A01352033@tec.mx>
2023-04-11 19:10:30 -0600

    usuarios model

--- Lab14/models/usuarios.model.js
+++ Lab14/models/usuarios.model.js
@@ -3,18 +3,18 @@ const users = [];
 
 module.exports = class Users {
 
-    //Constructor de la clase. Sirve para crear un nuevo objeto, y en él se definen las propiedades del modelo
+    //Constructor
     constructor(user) {
         this.username = user.username || '';
         this.password = user.password || '';
     }
 
-    //Este método servirá para guardar de manera persistente el nuevo objeto. 
+    //Este método es para guardar de manera persistente el nuevo objeto
     save() {
         users.push(this);
     }
 
-    //Este método servirá para devolver los objetos del almacenamiento persistente.
+    //Este método es para devolver los objetos del almacenamiento persistente
     static fetchAll() {
         return users;
     }
